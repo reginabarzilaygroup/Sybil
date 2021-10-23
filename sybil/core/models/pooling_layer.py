585513cf52d52233d106a826a0d125f7496954f7
PG Mikhael <pgmikhael@gmail.com>
2021-10-23 02:03:54 -0400

    clean main code

---
+++ sybil/core/models/pooling_layer.py
@@ -0,0 +1,179 @@
+import torch
+import torch.nn as nn
+
+class MultiAttentionPool(nn.Module):
+    def __init__(self):
+        super(MultiAttentionPool, self).__init__()
+        params = {
+            'num_chan': 512,
+            'conv_pool_kernel_size': 11,
+            'stride': 1
+            }
+        self.image_pool1 = Simple_AttentionPool_MultiImg(**params)
+        self.volume_pool1 = Simple_AttentionPool(**params)
+
+        self.image_pool2 = PerFrameMaxPool()
+        self.volume_pool2 = Conv1d_AttnPool(**params)
+
+        self.global_max_pool = GlobalMaxPool()
+
+        self.multi_img_hidden_fc = nn.Linear(2 * 512, 512)
+        self.hidden_fc = nn.Linear(3 * 512, 512)         
+
+    def forward(self, x):
+        #X dim: B, C, T, W, H
+        output = {}
+        
+        image_pool_out1 = self.image_pool1(x) # contains keys: "multi_image_hidden", "image_attention"
+        volume_pool_out1 = self.volume_pool1(image_pool_out1['multi_image_hidden'])  # contains keys: "hidden", "attention_scores"
+
+        image_pool_out2 = self.image_pool2(x) # contains keys: "multi_image_hidden"
+        volume_pool_out2 = self.volume_pool2(image_pool_out2['multi_image_hidden']) # contains keys: "hidden", "attention_scores"
+
+        for pool_out, num in [(image_pool_out1, 1), (volume_pool_out1, 1), (image_pool_out2, 2), (volume_pool_out2, 2) ]:
+            for key, val in pool_out.items():
+                output['{}_{}'.format(key, num)] = val
+    
+        maxpool_out = self.global_max_pool(x)
+        output['maxpool_hidden'] = maxpool_out['hidden']
+        
+        multi_image_hidden = torch.cat( [ image_pool_out1['multi_image_hidden'], image_pool_out2['multi_image_hidden']], dim = -2 )
+        output['multi_image_hidden'] = self.multi_img_fc(multi_image_hidden.permute([0,2,1]).contiguous()).permute([0,2,1]).contiguous()
+
+        hidden = torch.cat( [ volume_pool_out1['hidden'], volume_pool_out2['hidden'], output['maxpool_hidden']], dim = -1 )
+        output['hidden'] = self.hidden_fc(hidden)
+
+        return output 
+    
+class GlobalMaxPool(nn.Module):
+    '''
+    Pool to obtain the maximum value for each channel
+    '''
+    def __init__(self):
+        super(GlobalMaxPool, self).__init__()
+
+    def forward(self, x):
+        '''
+        args: 
+            - x: tensor of shape (B, C, T, W, H)
+        returns:
+            - output: dict. output['hidden'] is (B, C)
+        '''
+        spatially_flat_size = (*x.size()[:2], -1)
+        x = x.view(spatially_flat_size)
+        hidden, _ = torch.max(x, dim=-1)
+        return {'hidden': hidden}
+
+class PerFrameMaxPool(nn.Module):
+    '''
+    Pool to obtain the maximum value for each slice in 3D input 
+    '''
+    def __init__(self):
+        super(PerFrameMaxPool, self).__init__()
+    
+    def forward(self, x):
+        '''
+        args: 
+            - x: tensor of shape (B, C, T, W, H)
+        returns:
+            - output: dict. 
+                + output['multi_image_hidden'] is (B, C, T)
+        '''
+        assert len(x.shape) == 5
+        output = {}
+        spatially_flat_size = (*x.size()[:3], -1)
+        x = x.view(spatially_flat_size)
+        output['multi_image_hidden'], _ = torch.max(x, dim=-1)
+        return output
+
+class Conv1d_AttnPool(nn.Module):
+        '''
+        Pool to learn an attention over the slices after convolution
+        '''
+        def __init__(self, **kwargs):
+            super(Conv1d_AttnPool, self).__init__()
+            self.conv1d = nn.Conv1d(kwargs['num_chan'], kwargs['num_chan'], kernel_size= kwargs['conv_pool_kernel_size'], stride=kwargs['stride'], padding= kwargs['conv_pool_kernel_size']//2, bias=False)
+            self.aggregate = Simple_AttentionPool(**kwargs)
+        
+        def forward(self, x):
+            '''
+            args: 
+                - x: tensor of shape (B, C, T)
+            returns:
+                - output: dict
+                    + output['attention_scores']: tensor (B, C)
+                    + output['hidden']: tensor (B, C)
+            '''
+            # X: B, C, N
+            x = self.conv1d(x) # B, C, N'
+            return self.aggregate(x)
+
+class Simple_AttentionPool(nn.Module):
+    '''
+    Pool to learn an attention over the slices
+    '''
+    def __init__(self, **kwargs):
+        super(Simple_AttentionPool, self).__init__()
+
+        self.attention_fc = nn.Linear(kwargs['num_chan'], 1)
+        self.softmax = nn.Softmax(dim=-1)
+        self.logsoftmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+        '''
+        args: 
+            - x: tensor of shape (B, C, N)
+        returns:
+            - output: dict
+                + output['attention_scores']: tensor (B, C)
+                + output['hidden']: tensor (B, C)
+        '''
+        output = {}
+        B = x.shape[0]
+        spatially_flat_size = (*x.size()[:2], -1) #B, C, N
+
+        x = x.view(spatially_flat_size)
+        attention_scores = self.attention_fc(x.transpose(1,2)) #B, N, 1
+        
+        output['attention_scores'] = self.logsoftmax(attention_scores.transpose(1,2)).view(B, -1)
+        attention_scores = self.softmax( attention_scores.transpose(1,2)) #B, 1, N
+        
+        x = x * attention_scores #B, C, N
+        output['hidden'] = torch.sum(x, dim=-1)
+        return output
+
+class Simple_AttentionPool_MultiImg(nn.Module):
+    '''
+    Pool to learn an attention over the slices and the volume
+    '''
+    def __init__(self, **kwargs):
+        super(Simple_AttentionPool_MultiImg, self).__init__(args, kwargs['num_chan'])
+
+        self.attention_fc = nn.Linear(kwargs['num_chan'], 1)
+        self.softmax = nn.Softmax(dim=-1)
+        self.logsoftmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+        '''
+        args: 
+            - x: tensor of shape (B, C, T, W, H)
+        returns:
+            - output: dict
+                + output['attention_scores']: tensor (B, T, C)
+                + output['multi_image_hidden']: tensor (B, T, C)
+                + output['hidden']: tensor (B, T*C)
+        '''
+        output = {} 
+        B, C, T, W, H = x.size()
+        x = x.permute([0,2,1,3,4])
+        x = x.contiguous().view(B*T, C, W*H)
+        attention_scores = self.attention_fc(x.transpose(1,2)) #BT, WH , 1
+        
+        output['attention_scores'] = self.logsoftmax(attention_scores.transpose(1,2)).view(B, T, -1) 
+        attention_scores = self.softmax( attention_scores.transpose(1,2)) #BT, 1, WH
+        
+        x = x * attention_scores #BT, C, WH
+        x = torch.sum(x, dim=-1)
+        output['multi_image_hidden'] = x.view(B, T, C).permute([0,2,1]).contiguous()
+        output['hidden'] = x.view(B, T * C)
+        return output
--- sybil/core/models/sybil.py
+++ sybil/core/models/sybil.py
@@ -1,78 +1,49 @@
-from sandstone.models.factory import load_model, RegisterModel, get_model_by_name
-from sandstone.models.pools.factory import get_pool
-import math
 import torch
 import torch.nn as nn
-import pdb
-import numpy as np
-import copy
-from models.cumulative_probability_layer import Cumulative_Probability_Layer
-import time
-import json
-import torchvision
 import torch.nn.functional as F
-from sandstone.utils.nlst_risk_factors import NLSTRiskFactorVectorizer
-import warnings
-import pdb
+import torchvision
+from models.cumulative_probability_layer import Cumulative_Probability_Layer
+from models.pooling_layer import MultiAttentionPool
+from datasets.nlst_risk_factors import NLSTRiskFactorVectorizer
 
-@RegisterModel("r3d")
-class R3D18(nn.Module):
+class Sybil(nn.Module):
     def __init__(self, args):
-        super(R3D18, self).__init__()
+        super(Sybil, self).__init__()
 
-        self.args = args
-        encoder = torchvision.models.video.r3d_18(pretrained=args.pretrained_on_imagenet)
-        num_features = encoder.fc.in_features
+        self.hidden_dim = 512
+
+        encoder = torchvision.models.video.r3d_18(pretrained=True)
         self.image_encoder = nn.Sequential(*list(encoder.children())[:-2])
 
-        pool_name = args.pool_name
-        self.pool = get_pool(pool_name)(args, args.hidden_dim)
-        if not self.pool.replaces_fc():
-            # Cannot not placed on self.all_blocks since requires intermediate op
-            self.relu = nn.ReLU(inplace=False)
-            self.dropout = nn.Dropout(p=args.dropout)
-            self.fc = nn.Linear(args.hidden_dim, args.num_classes)
+        self.pool = MultiAttentionPool()
+
+        self.relu = nn.ReLU(inplace=False)
+        self.dropout = nn.Dropout(p=args.dropout)
+        self.fc = nn.Linear(args.hidden_dim, args.num_classes)
         
-        if args.survival_analysis_setup and not args.eval_risk_on_survival:
-            self.prob_of_failure_layer = Cumulative_Probability_Layer(args.hidden_dim, args, max_followup=args.max_followup)
+        self.prob_of_failure_layer = Cumulative_Probability_Layer(args.hidden_dim, args, max_followup=args.max_followup)
         
-        if args.use_annotations:
-           assert args.region_annotations_filepath, 'ANNOTATIONS METADATA FILE NOT SPECIFIED'
-           self.annotations_metadata = json.load(open(args.region_annotations_filepath, 'r'))
-        
-        
-    def forward(self, x, batch=None):
+    def forward(self, x):
         output = {}
         x = self.image_encoder(x)
-        pool_output = self.aggregate_and_classify(x, batch=batch)
+        pool_output = self.aggregate_and_classify(x)
         output['activ'] = x
         output.update(pool_output)
 
         return output
 
-    def aggregate_and_classify(self, x, batch):
+    def aggregate_and_classify(self, x):
+        pool_output =  self.pool(x)
         
-        pool_output =  self.pool(x, batch)
+        pool_output['hidden'] = self.relu(pool_output['hidden'])
+        pool_output['hidden'] = self.dropout(pool_output['hidden'])
+        pool_output['logit']  = self.fc(pool_output['hidden'])
 
-        for pool_name in self.pool.pool_names:
-            if self.args.multipool_pools is not None:
-                hidden_key, logit_key = '{}_hidden'.format(pool_name), '{}_logit'.format(pool_name)
-            else:
-                hidden_key, logit_key = 'hidden', 'logit'
-            
-            if not self.pool.replaces_fc():
-                pool_output[hidden_key] = self.relu(pool_output[hidden_key])
-                pool_output[hidden_key] = self.dropout(pool_output[hidden_key])
-                pool_output[logit_key]  = self.fc(pool_output[hidden_key])
-
-            if self.args.survival_analysis_setup and not (hasattr(self.args, 'eval_risk_on_survival') and  self.args.eval_risk_on_survival):
-                pool_output[logit_key] = self.prob_of_failure_layer(pool_output[hidden_key])
-            
+        pool_output['logit'] = self.prob_of_failure_layer(pool_output['hidden'])
+        
         return pool_output
 
-
-@RegisterModel("risk_factors_predictor")
-class RiskFactorPredictor(R3D18):
+class RiskFactorPredictor(Sybil):
     def __init__(self, args):
         super(RiskFactorPredictor, self).__init__(args)
 
@@ -86,19 +57,12 @@ class RiskFactorPredictor(R3D18):
         output = {}
         x = self.image_encoder(x)
         output = self.pool(x, batch)
-        
-        if self.args.multipool_pools is not None:
-            hidden_key, logit_key = '{}_hidden'.format(self.pool.pool_names[0]), '{}_logit'.format(self.pool.pool_names[0])
-        else:
-            hidden_key, logit_key = 'hidden', 'logit'
-        
-        hidden = output[hidden_key]
-
-        risk_factors = batch['risk_factors']
+    
+        hidden = output['hidden']
         for indx, key in enumerate(self.args.risk_factor_keys):
             output['{}_logit'.format(key)] = self._modules['{}_fc'.format(key)](hidden)
 
         return output
 
-    def get_loss_functions(self, args):
+    def get_loss_functions(self):
         return ['risk_factor_loss']
