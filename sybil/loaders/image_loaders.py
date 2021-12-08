c15fe4f825275e2c5c53b23a09855d09c9d64fa3
Jeremy Wohlwend <jeremy.wohlwend@gmail.com>
2021-12-08 10:16:08 -0500

    update

--- sybil/loaders/image_loaders.py
+++ sybil/loaders/image_loaders.py
@@ -1,30 +1,25 @@
-from sandstone.datasets.loader.factory import RegisterInputLoader
-from sandstone.datasets.loader.abstract_loader import abstract_loader
+from sybil.loaders.abstract_loader import abstract_loader
 import cv2
-import numpy as np
-import pydicom
-import os
-import math
-import torch 
+import torch
 
-LOADING_ERROR = 'LOADING ERROR! {}'
+LOADING_ERROR = "LOADING ERROR! {}"
 
-@RegisterInputLoader('default_image_loader')
+
 class OpenCVLoader(abstract_loader):
     def configure_path(self, path, additional, sample):
         return path
 
     def load_input(self, path, additional):
-        '''
+        """
         loads as grayscale image
-        '''
+        """
         return cv2.imread(path, 0)
 
     @property
     def cached_extension(self):
-        return '.png'
+        return ".png"
 
-@RegisterInputLoader('tensor_loader')
+
 class TensorLoader(abstract_loader):
     def configure_path(self, path, additional, sample):
         return path
@@ -32,13 +27,13 @@ class TensorLoader(abstract_loader):
     def load_input(self, path, additional):
         try:
             arr = torch.load(path)
-        except:
-            raise Exception(LOADING_ERROR.format('COULD NOT LOAD TENSOR.'))
+        except Exception:
+            raise Exception(LOADING_ERROR.format("COULD NOT LOAD TENSOR."))
         return arr
 
     @property
     def cached_extension(self):
-        return ''
+        return ""
 
     @property
     def apply_augmentations(self):
--- sybil/models/cumulative_probability_layer.py
+++ sybil/models/cumulative_probability_layer.py
@@ -1,20 +1,18 @@
 import torch
 import torch.nn as nn
-import pdb
-
 
 
 class Cumulative_Probability_Layer(nn.Module):
     def __init__(self, num_features, args, max_followup):
         super(Cumulative_Probability_Layer, self).__init__()
         self.args = args
-        self.hazard_fc = nn.Linear(num_features,  max_followup)
+        self.hazard_fc = nn.Linear(num_features, max_followup)
         self.base_hazard_fc = nn.Linear(num_features, 1)
         self.relu = nn.ReLU(inplace=True)
         mask = torch.ones([max_followup, max_followup])
         mask = torch.tril(mask, diagonal=0)
         mask = torch.nn.Parameter(torch.t(mask), requires_grad=False)
-        self.register_parameter('upper_triagular_mask', mask)
+        self.register_parameter("upper_triagular_mask", mask)
 
     def hazards(self, x):
         raw_hazard = self.hazard_fc(x)
@@ -23,9 +21,13 @@ class Cumulative_Probability_Layer(nn.Module):
 
     def forward(self, x):
         hazards = self.hazards(x)
-        B, T = hazards.size() #hazards is (B, T)
-        expanded_hazards = hazards.unsqueeze(-1).expand(B, T, T) #expanded_hazards is (B,T, T)
-        masked_hazards = expanded_hazards * self.upper_triagular_mask # masked_hazards now (B,T, T)
+        B, T = hazards.size()  # hazards is (B, T)
+        expanded_hazards = hazards.unsqueeze(-1).expand(
+            B, T, T
+        )  # expanded_hazards is (B,T, T)
+        masked_hazards = (
+            expanded_hazards * self.upper_triagular_mask
+        )  # masked_hazards now (B,T, T)
         base_hazard = self.base_hazard_fc(x)
         cum_prob = torch.sum(masked_hazards, dim=1) + base_hazard
         return cum_prob
