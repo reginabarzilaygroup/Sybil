660768cbb1bb00f566a0acb3bdee26f8936d41c7
PG Mikhael <pgmikhael@gmail.com>
2021-12-11 18:07:41 -0500

    add dicom loader

--- sybil/loaders/image_loaders.py
+++ sybil/loaders/image_loaders.py
@@ -2,6 +2,8 @@ from sybil.loaders.abstract_loader import abstract_loader
 import cv2
 import torch
 import pydicom
+from pydicom.pixel_data_handlers.util import apply_modality_lut
+import numpy as np 
 
 LOADING_ERROR = "LOADING ERROR! {}"
 
@@ -22,13 +24,19 @@ class OpenCVLoader(abstract_loader):
 
 
 class DicomLoader(abstract_loader):
+    def __init__(self, cache_path, augmentations, args):
+        super(DicomLoader, self).__init__(cache_path, augmentations, args)
+        self.window_center = -600
+        self.window_width  = 1500
+
     def configure_path(self, path, additional, sample):
         return path
 
     def load_input(self, path, additional):
         try:
             dcm = pydicom.dcmread(path)
-            arr = dcm.pixel_array
+            dcm = apply_modality_lut(dcm.pixel_array, dcm)
+            arr = apply_windowing(dcm, self.window_center, self.window_width)
         except Exception:
             raise Exception(LOADING_ERROR.format("COULD NOT LOAD DICOM."))
         return arr
@@ -46,3 +54,38 @@ class DicomLoader(abstract_loader):
         rp = (self.args.num_chan, 1, 1, 1)
         images = images.unsqueeze(0).repeat(rp)
         return images
+
+def apply_windowing(image, center, width, bit_size=16):
+    """Windowing function to transform image pixels for presentation.
+    Must be run after a DICOM modality LUT is applied to the image.
+    Windowing algorithm defined in DICOM standard:
+    http://dicom.nema.org/medical/dicom/2020b/output/chtml/part03/sect_C.11.2.html#sect_C.11.2.1.2
+    Reference implementation:
+    https://github.com/pydicom/pydicom/blob/da556e33b/pydicom/pixel_data_handlers/util.py#L460
+    Args:
+        image (ndarray): Numpy image array
+        center (float): Window center (or level)
+        width (float): Window width
+        bit_size (int): Max bit size of pixel
+    Returns:
+        ndarray: Numpy array of transformed images
+    """
+    y_min = 0
+    y_max = (2**bit_size - 1)
+    y_range = y_max - y_min
+
+    c = center - 0.5
+    w = width - 1
+
+    below = image <= (c - w / 2)  # pixels to be set as black
+    above = image > (c + w / 2)  # pixels to be set as white
+    between = np.logical_and(~below, ~above)
+
+    image[below] = y_min
+    image[above] = y_max
+    if between.any():
+        image[between] = (
+                ((image[between] - c) / w + 0.5) * y_range + y_min
+        )
+
+    return image
