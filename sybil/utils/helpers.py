28247ac45446159cdbf7600f44c5f91a9992a0d1
PG Mikhael <pgmikhael@gmail.com>
2022-01-17 01:07:34 -0500

    add mgh datasets

--- sybil/utils/helpers.py
+++ sybil/utils/helpers.py
@@ -1,11 +1,15 @@
 from sybil.datasets.sybil import SybilDataset
 from sybil.datasets.nlst import *
+from sybil.datasets.mgh import MGH_Dataset
+from sybil.datasets.joint_nlst_mgh import MGH_NLST_Combined_Dataset
 
 SUPPORTED_DATASETS = {
     'sybil': SybilDataset,
     'nlst': NLST_Survival_Dataset,
     'nlst_risk_factors': NLST_Risk_Factor_Task,
-    'nlst_for_plco': NLST_for_PLCO
+    'nlst_for_plco': NLST_for_PLCO,
+    'mgh': MGH_Dataset,
+    'nlst_mgh': MGH_NLST_Combined_Dataset
 }
 
 def get_dataset(dataset_name, split, args):
